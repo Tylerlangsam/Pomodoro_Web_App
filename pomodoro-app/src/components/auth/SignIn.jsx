fadea525c4272603c123a6b1fd85dab9d590f1a1
Tylerlangsam <90009414+Tylerlangsam@users.noreply.github.com>
2023-10-03 14:42:32 -0700

    Modifying auth components

--- pomodoro-app/src/components/auth/SignIn.jsx
+++ pomodoro-app/src/components/auth/SignIn.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from "react";
-import { auth } from '../../firebase';
+import { auth } from "../../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import NavBar from "../NavBar";
+import { useHistory } from "react-router-dom"; 
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const history = useHistory();
 
   const signIn = (e) => {
     e.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
+      .then((userCredential) => {
+        // Redirect the user to the "/pomodoro" route
+        history.push("/pomodoro");
+      })
+      .catch((error) => {
+        // Handle sign-in errors if needed
+        console.error(error);
+      });
+  };
 
-    })
-  }
   return (
     <div className="sign-in-container">
-        <NavBar></NavBar>
+      <NavBar />
       <form onSubmit={signIn}>
         <h1>Log In to your Account</h1>
         <input
@@ -30,7 +38,7 @@ const SignIn = () => {
           placeholder="Enter your password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}></input>
-          <button type="submit">Log In</button>
+        <button type="submit">Log In</button>
       </form>
     </div>
   );
--- pomodoro-app/src/components/auth/SignUp.jsx
+++ pomodoro-app/src/components/auth/SignUp.jsx
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
-import NavBar from "../NavBar";
-import { auth } from '../../firebase';
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "../../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const history = useHistory();
 
   const signUp = (e) => {
     e.preventDefault();
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
+      .then((userCredential) => {
+        // Redirect to "/pomodoro" on successful sign-up
+        history.push("/pomodoro");
+      })
+      .catch((error) => {
+        // Handle sign-up errors if needed
+        console.error("Sign-up error:", error);
+      });
+  };
 
-    })
-  }
   return (
     <div className="sign-in-container">
-        <NavBar></NavBar>
       <form onSubmit={signUp}>
         <h1>Create Account</h1>
         <input
           type="email"
           placeholder="Enter your email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}></input>
-
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <input
           type="password"
           placeholder="Enter your password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}></input>
-          <button type="submit">Sign Up</button>
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign Up</button>
       </form>
+      <p>
+        Already have an account? <Link to="/login">Log In</Link>
+      </p>
     </div>
   );
 };
